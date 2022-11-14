0722259ec006d8d29a7a0cc96761f4f024e2017c
mjk1376 <50069292+mjk1376@users.noreply.github.com>
2022-11-14 11:31:21 +0330

    Rename Lesson_two_led_blinking_outside_the_board.ino to Lesson_2_led_blinking_outside_the_board.ino

---
+++ Lesson_2_led_blinking_outside_the_board.ino
@@ -0,0 +1,17 @@
+int Pin=12;
+int delay_time=1000;
+
+void setup() {
+// put your setup code here, to run once:
+pinMode(Pin,OUTPUT);
+
+}
+
+void loop() {
+// put your main code here, to run repeatedly:
+
+digitalWrite(Pin,HIGH);
+delay(delay_time);
+digitalWrite(Pin,LOW);
+delay(delay_time);
+}
